5439cbb01d6ee0cd03c95d6ac2f60679f048e45f
Leandro Koshoji Araujo <le.kos@hotmail.com>
2021-06-30 21:43:37 -0300

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 const display = document.querySelector('[data-js="display"]')
 const auxDisplay = document.querySelector('[data-js="display-aux"]')
-const numbersButtons = document.querySelectorAll('[data-js="number"]')
-const operationButtons = document.querySelectorAll('[data-js="operator"]')
-const equalButton = document.querySelector('[data-js="equal"]')
-const clearAllButton = document.querySelector('[data-js="clearAll"]')
+const keys = document.querySelector('.calculator__keys')
 
 let firstValue = 0
 let secondValue = 0
@@ -14,8 +11,53 @@ const doOperation = (firstValue, operator, secondValue) => {
     '+': firstValue + secondValue,
     '-': firstValue - secondValue,
     '/': firstValue / secondValue,
-    'x': firstValue * secondValue,
+    x: firstValue * secondValue,
     '%': firstValue * 0.01,
   }
   return operations[operator]
 }
+
+const clearAll = () => {
+  display.textContent = ''
+  auxDisplay.textContent = ''
+  firstValue = 0
+  secondValue = 0
+  operator = ''
+}
+
+keys.addEventListener('click', event => {
+  const clickedEl = event.target
+  const isDataNumber = clickedEl.dataset.js === 'number'
+  const isDataOperator = clickedEl.dataset.js === 'operator'
+  const isDataEqual = clickedEl.dataset.js === 'equal'
+  const isDataClearAll = clickedEl.dataset.js === 'clearAll'
+
+  if (isDataNumber) {
+    const isDotInserted = clickedEl.textContent === '.' && display.textContent.includes('.')
+
+    if (isDotInserted) return
+
+    display.textContent += clickedEl.textContent
+  }
+
+  if (isDataOperator) {
+    const isDisplayEmpty = display.textContent === ''
+    if (isDisplayEmpty) return
+
+    firstValue = Number(display.textContent)
+    operator = clickedEl.textContent
+    auxDisplay.textContent = firstValue + operator
+    display.textContent = ''
+  }
+
+  if (isDataEqual) {
+    secondValue = Number(display.textContent)
+    auxDisplay.textContent += secondValue
+    display.textContent = doOperation(firstValue, operator, secondValue)
+    firstValue = display.textContent
+  }
+
+  if (isDataClearAll) {
+    clearAll()
+  }
+})
